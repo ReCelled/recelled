782d8a06aff0a696c9f3cf31c965d34c371491e0
yofukashino <alokender018@gmail.com>
2025-03-10 16:44:05 +0530

    chore: remove unused

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,6 @@ export const WEBLATE_URL = "https://i18n.replugged.dev";
 export const WEBSITE_URL = "https://recelled.dev";
 export const BETA_WEBSITE_URL = "https://beta.recelled.dev";
 export const LEGACY_WEBSITE_URL = "https://replugged.dev"; // requirred for web store to work!
-export const LEGACY_BETA_WEBSITE_URL = "https://beta.replugged.dev";
 export const DISCORD_BLURPLE = "#5865f2";
 export const RECELLED_CLYDE_ID = "8269676976766968"; // "RECELLED" in ASCII codes
 export const DEFAULT_LOCALE = "en-US";
